8b20eff0ffa931506ad652bcf1617b2cf31096c5
Enzo <86987017+passos-enzo@users.noreply.github.com>
2024-12-03 11:04:13 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -1,6 +1,9 @@
+let x = 2
+
 console.log("---- var ----");
 if (true) {
     var exemploVar = "Visível fora do bloco!";
+    console.log(x)
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
 
@@ -9,16 +12,16 @@ if (true) {
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+console.log(exemploLet); // Erro: exemploLet não está definido.
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
-// PI = 3.15; // Erro: Não é permitido alterar uma const
+PI = 3.15; // Erro: Não é permitido alterar uma const
 
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
 escopoLocal();
-// console.log(local); // Erro: 'local' é inacessível fora da função
+console.log(local); // Erro: 'local' é inacessível fora da função
