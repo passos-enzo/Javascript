a1eef117c82abbc0ddf2d260feee561593f8d63c
enzo <passosenzo2007@gmail.com>
2024-12-06 09:03:29 -0300

    Teste de Escopo

--- variaveis.js
+++ variaveis.js
@@ -1,16 +1,22 @@
+var nome = "Enzo";
+console.log(nome);
+
 console.log("---- var ----");
 if (true) {
+    console.log("Acessível dentro do bloco: " + nome);
     var exemploVar = "Visível fora do bloco!";
 }
 console.log(exemploVar); // Funciona, mas pode causar problemas.
 
 console.log("---- let ----");
+let sobrenome = "Scandolara";
 if (true) {
+    console.log(sobrenome); // Funciona aqui.
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
-
+//console.log(exemploLet); // Erro: exemploLet não está definido.
+console.log("Acessível globalmente: " + sobrenome);
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
